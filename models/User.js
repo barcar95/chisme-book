aac545df33789aa6d03caefaa4ba3a3ab5e449cb
barre <barrenac@oregonstate.edu>
2023-09-08 13:06:54 -0700

    fixed package.json string issue, created User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: { type: String, unique: true,  required: true, trim: true},
+        // email: { type: String, required: true, unique: true, },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+    }
+);
+
+// virtual property `friendCount` that retrieves the length of the user's friends on query
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return `Friend Count = ${this.friends.length}`
+    });
+
+    const User = model('user', userSchema);
+
+    module.exports = User;
